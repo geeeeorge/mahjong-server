1e13ffb86ef68fdbe393c741ade176e6f943ba6d
geeeeorge <yuto.george@i.softbank.jp>
2024-02-13 07:20:15 +0900

    fix: apigateway websocketが動くように

--- terraform/modules/apigateway-route/main.tf
+++ terraform/modules/apigateway-route/main.tf
@@ -1,11 +1,7 @@
 resource "aws_apigatewayv2_integration" "gateway_integration" {
   api_id                    = var.gateway_id
   integration_type          = "AWS_PROXY"
-  connection_type           = "INTERNET"
-  content_handling_strategy = "CONVERT_TO_TEXT"
-  integration_method        = "POST"
   integration_uri           = var.invoke_arn
-  passthrough_behavior      = "WHEN_NO_MATCH"
 }
 
 resource "aws_apigatewayv2_route" "gateway_route" {
--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -8,4 +8,30 @@ resource "aws_apigatewayv2_stage" "websocket_stage" {
   api_id      = aws_apigatewayv2_api.websocket_gw.id
   name        = var.env
   auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.apigateway_access_log.arn
+    format = jsonencode({
+      "aws_endpoint_request_id" : "$context.awsEndpointRequestId",
+      "request_id" : "$context.requestId",
+      "ip" : "$context.identity.sourceIp",
+      "request_time" : "$context.requestTime",
+      "http_method" : "$context.httpMethod",
+      "resource_path" : "$context.resourcePath",
+      "status" : "$context.status",
+      "protocol" : "$context.protocol",
+      "response_length" : "$context.responseLength",
+      "stage" : "$context.stage",
+      "message" : "$context.error.message",
+      "extended_request_id" : "$context.extendedRequestId",
+      "validation_error_string" : "$context.error.validationErrorString",
+      "integration_status" : "$context.integration.status",
+      "integration_error" : "$context.integration.error",
+    })
+  }
+}
+
+resource "aws_cloudwatch_log_group" "apigateway_access_log" {
+  name              = "/aws/apigateway/${var.gateway_name}_${var.env}_access_log"
+  retention_in_days = 30
 }
